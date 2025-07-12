f2943a2613f888980a881a680ce8099f48373f93
Umarova Albina <albinaumarova2014@gmail.com>
2025-07-13 02:07:15 +0600

    Simple test

--- api/config.ts
+++ api/config.ts
@@ -5,11 +5,11 @@ const rootPath = __dirname;
 const config = {
     rootPath,
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
     publicPath: path.join(rootPath, 'public'),
-    db: 'mongodb://localhost/shop-js-27'
+    db: process.env.MONGO_URL || 'mongodb://localhost',
 };
 
 export default config;
