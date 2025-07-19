52ffcec1f3d368c454e1da9daad7d13269dc998e
Umarova Albina <albinaumarova2014@gmail.com>
2025-07-19 11:57:08 +0600

    test

--- api/fizzBuzz/fizzBuzz.test.ts
+++ api/fizzBuzz/fizzBuzz.test.ts
@@ -12,7 +12,7 @@ describe('fizzBuzz function', () => {
 
     it('return 1 for 1', () => {
         const result = fizzBuzz(1);
-        expect(result).toBe(2);
+        expect(result).toBe(1);
     });
 
     it('return 2 for 2', () => {
